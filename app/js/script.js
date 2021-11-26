145451a855f4cb9d66117a996d3c91d16dad6a5a
Mike <nux7jr@gmail.com>
2021-11-26 21:41:21 +0700

    1.0.1

---
+++ app/js/script.js
@@ -0,0 +1,14 @@
+const testString = 'This is a test!';
+
+console.log(testString);
+
+const fruits = [
+  'mango',
+  'papaya',
+  'lychee',
+];
+
+fruits.map((fruit) => {
+  console.log(fruit);
+});
+
